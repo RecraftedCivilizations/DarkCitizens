98f03245897614490c86849b1b86a8e32c263b0c
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-01-16 18:20:59 +0100

    feat: Create a dPlayer factory

---
+++ src/main/kotlin/com/github/recraftedcivilizations/DPlayer/DPlayerFactory.kt
@@ -0,0 +1,22 @@
+package com.github.recraftedcivilizations.DPlayer
+
+import com.github.recraftedcivilizations.jobs.IJob
+import org.bukkit.entity.Player
+import java.util.*
+
+object DPlayerFactory {
+
+    fun createDPlayer(uuid: UUID, job: IJob?, wanted: Boolean,isCriminal: Boolean, groupLvls: MutableMap<String, Int>, groupXps: MutableMap<String, Int>): DPlayer{
+        val dData = DPlayerData1(uuid, job, wanted ,isCriminal , groupLvls , groupXps)
+        return DPlayer(dData)
+    }
+
+    fun createDPLayer(job: IJob?, wanted: Boolean,isCriminal: Boolean, groupLvls: MutableMap<String, Int>, groupXps: MutableMap<String, Int>, player: Player): DPlayer{
+        val dData = DPlayerData2(job, wanted ,isCriminal , groupLvls , groupXps)
+        return DPlayer(dData, player)
+    }
+
+    fun createDPlayer(player: Player): DPlayer{
+        return DPlayer(player)
+    }
+}
