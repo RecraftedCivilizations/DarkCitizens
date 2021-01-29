3a7f4245a20c868f9613a7860d901506eaa1498c
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-01-29 12:42:55 +0100

    feat: Test the DPlayerManager

---
+++ src/test/kotlin/com/github/recraftedcivilizations/dPlayer/DPlayerManagerTest.kt
@@ -0,0 +1,46 @@
+package com.github.recraftedcivilizations.dPlayer
+
+import com.github.recraftedcivilizations.parser.dataparser.IParseData
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import org.bukkit.entity.Player
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.mockito.Mockito
+import org.mockito.stubbing.Answer
+import java.util.*
+
+internal class DPlayerManagerTest {
+    val uuid = UUID.randomUUID()
+    val mockPlayer = mock<Player> {on { getUniqueId() } doReturn uuid}
+    val dPlayerData = DPlayerData1(
+        uuid,
+        null,
+        true,
+        true,
+        mapOf(Pair("Foo", 3), Pair("", 10)),
+        mapOf(Pair("Foo", 3), Pair("", 10))
+    )
+    val mockDPlayer = DPlayer(dPlayerData)
+    val dataParser = mock<IParseData> { on { getDPlayer(uuid) } doReturn mockDPlayer}
+
+    @Test
+    fun getDPlayer() {
+        val dPlayerManager = DPlayerManager(dataParser)
+        val dplayer = dPlayerManager.getDPlayer(uuid)
+        assertEquals(mockDPlayer, dplayer)
+    }
+
+    @Test
+    fun testGetDPlayer() {
+        val dPlayerManager = DPlayerManager(dataParser)
+        val dplayer = dPlayerManager.getDPlayer(mockPlayer)
+        assertEquals(mockDPlayer, dplayer)
+    }
+
+    @Test
+    fun shouldConstruct(){
+        val dPlayerManager = DPlayerManager(dataParser)
+    }
+}
