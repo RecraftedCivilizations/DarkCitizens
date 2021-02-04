58237fd53df1eb304b77cd1ce97580c9ee95ac1d
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-02-04 15:50:47 +0100

    feat: Add logging to the BukkitWrapper

--- src/main/kotlin/com/github/recraftedcivilizations/BukkitWrapper.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/BukkitWrapper.kt
@@ -4,6 +4,7 @@ import com.github.recraftedcivilizations.dPlayer.DPlayer
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import java.util.*
+import java.util.logging.Level
 
 /**
  * @author DarkVanityOfLight
@@ -15,6 +16,8 @@ import java.util.*
  * this is mainly for testing purposes
  */
 open class BukkitWrapper{
+
+    private val logger = Bukkit.getLogger()
 
     /**
      * Get a Bukkit player from a [DPlayer]
@@ -30,5 +33,33 @@ open class BukkitWrapper{
      */
     open fun getPlayer(uuid: UUID): Player? {
         return Bukkit.getPlayer(uuid)
+    }
+
+    open fun info(message: String?) {
+        logger.log(Level.INFO, message)
+    }
+
+    open fun info(message: String?, vararg vars: Any?) {
+        logger.log(Level.INFO, message, vars)
+    }
+
+    open fun severe(message: String?) {
+        logger.log(Level.SEVERE, message)
+    }
+
+    open fun severe(message: String?, error: Throwable?) {
+        logger.log(Level.SEVERE, message, error)
+    }
+
+    open fun warning(message: String?) {
+        logger.log(Level.WARNING, message)
+    }
+
+    open fun warning(message: String?, error: Throwable?) {
+        logger.log(Level.WARNING, message, error)
+    }
+
+    open fun warning(message: String?, vararg vars: Any?) {
+        logger.log(Level.WARNING, message, vars)
     }
 }
