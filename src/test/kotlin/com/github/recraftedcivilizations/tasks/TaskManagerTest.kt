361d04386f738ce8d5042ae3675ba36fe4b903dc
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-02-05 13:58:55 +0100

    feat: Test the TaskManager

---
+++ src/test/kotlin/com/github/recraftedcivilizations/tasks/TaskManagerTest.kt
@@ -0,0 +1,70 @@
+package com.github.recraftedcivilizations.tasks
+
+import com.github.recraftedcivilizations.dPlayer.DPlayerManager
+import com.github.recraftedcivilizations.jobs.randomString
+import com.github.recraftedcivilizations.parser.dataparser.IParseData
+import com.github.recraftedcivilizations.tasks.actions.Actions
+import com.github.recraftedcivilizations.tasks.actions.IAction
+import com.nhaarman.mockitokotlin2.mock
+import net.milkbowl.vault.economy.Economy
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import kotlin.random.Random
+
+internal class TaskManagerTest {
+
+    val econ = mock<Economy>{}
+    val dataParser = mock<IParseData>{}
+    val dPlayerManager = DPlayerManager(dataParser)
+    val action = mock<IAction>{}
+
+    val taskField = TaskManager::class.java.getDeclaredField("tasks")
+
+    init {
+        taskField.isAccessible =true
+    }
+
+    @Test
+    fun shouldConstruct(){
+        val taskManager = TaskManager(econ, dPlayerManager)
+    }
+
+    @Test
+    fun createTask() {
+        val taskManager = TaskManager(econ, dPlayerManager)
+        val task1 = Task(randomString(), Random.nextInt(), Random.nextInt(), listOf(Actions.DEBUG), randomString(), dPlayerManager, econ)
+        taskManager.createTask(task1.name, task1.income, task1.xp, listOf("DEBUG", "FOO"), task1.description)
+
+        val tasks = taskField.get(taskManager) as Set<*>
+
+        assertEquals(1, tasks.size)
+        val task2 = tasks.iterator().next() as ITask
+
+        assertEquals(task1.name, task2.name)
+        assertEquals(task1.income, task2.income)
+        assertEquals(task1.xp, task2.xp)
+        assertEquals(task1.actions[0], task2.actions[0])
+        assertEquals(task1.actions.size, task2.actions.size)
+        assertEquals(task1.description, task2.description)
+
+    }
+
+    @Test
+    fun getTask() {
+        val taskManager = TaskManager(econ, dPlayerManager)
+        val task1 = Task(randomString(), Random.nextInt(), Random.nextInt(), listOf(Actions.DEBUG), randomString(), dPlayerManager, econ)
+        taskManager.createTask(task1.name, task1.income, task1.xp, listOf("DEBUG"), task1.description)
+        val task2 = taskManager.getTask(task1.name)
+
+
+        assertEquals(task1.name, task2?.name)
+        assertEquals(task1.income, task2?.income)
+        assertEquals(task1.xp, task2?.xp)
+        assertEquals(task1.actions[0], task2?.actions?.get(0))
+        assertEquals(task1.actions.size, task2?.actions?.size)
+        assertEquals(task1.description, task2?.description)
+
+        assertEquals(null, taskManager.getTask("FOOBAR"))
+    }
+}
