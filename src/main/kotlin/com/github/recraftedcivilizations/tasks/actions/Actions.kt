00cd6141c68ac7e24595f9032de355db168c8f4c
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-02-05 13:58:32 +0100

    feat: Create a DEBUG Action

--- src/main/kotlin/com/github/recraftedcivilizations/tasks/actions/Actions.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/tasks/actions/Actions.kt
@@ -1,5 +1,8 @@
 package com.github.recraftedcivilizations.tasks.actions
 
+import com.github.recraftedcivilizations.dPlayer.DPlayer
+import org.bukkit.entity.Player
+
 /**
  * @author DarkVanityOfLight
  */
@@ -7,4 +10,19 @@ package com.github.recraftedcivilizations.tasks.actions
 /**
  * All actions have to be registered here
  */
-enum class Actions : IAction {}
+enum class Actions : IAction {
+
+    DEBUG {
+        override val description: String
+            get() = "Foo"
+
+        override fun isCompletedForPlayer(player: DPlayer): Boolean {
+            return true
+        }
+
+        override fun isCompletedForPlayer(player: Player): Boolean {
+            return true
+        }
+    }
+
+}
