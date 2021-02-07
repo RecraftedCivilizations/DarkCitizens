fb7e36ef35adcd21db83c0dada3c563d290d36c8
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-02-07 17:28:49 +0100

    chore: Docs

--- src/main/kotlin/com/github/recraftedcivilizations/parser/dataparser/YMLDataSource.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/parser/dataparser/YMLDataSource.kt
@@ -10,9 +10,21 @@ import org.bukkit.configuration.file.YamlConfiguration
 import java.io.File
 import java.util.*
 
+/**
+ * @author DarkVanityOfLight
+ */
+
+/**
+ * Save and read data from a YML file
+ * @see IParseData
+ * @constructor Construct using a base [filePath] and an optional bukkitWrapper for testing
+ */
 class YMLDataSource(var filePath: String, val bukkitWrapper: BukkitWrapper = BukkitWrapper()) : IParseData {
     private val dataFile : YamlConfiguration = YamlConfiguration()
 
+    /**
+     * Store names and paths for the config file
+     */
     companion object{
         const val fileName = "data.yml"
         const val dPlayerDataPath = "DPlayerData"
@@ -39,6 +51,10 @@ class YMLDataSource(var filePath: String, val bukkitWrapper: BukkitWrapper = Buk
         }
     }
 
+    /**
+     * Get a DPlayer from the DataSource
+     * @param playerUUID The UUID of the player you want to get
+     */
     override fun getDPlayer(playerUUID: UUID): DPlayer {
         val configSection = dataFile.getConfigurationSection("$dPlayerDataPath.$playerUUID")
 
@@ -51,14 +67,27 @@ class YMLDataSource(var filePath: String, val bukkitWrapper: BukkitWrapper = Buk
         return dPlayer ?: DPlayerFactory.createDPlayer(bukkitWrapper.getPlayer(playerUUID)!!)
     }
 
+    /**
+     * Set a DPlayer in the DataSource
+     * @param dPlayer The player to save to the DataSource
+     */
     override fun setDPlayer(dPlayer: DPlayer) {
         setDPlayer(dPlayer.serializeData())
     }
 
+    /**
+     * Set a DPlayer from dPlayerData of the form [DPlayerData1]
+     * @param dData The dPlayerData of the player
+     */
     override fun setDPlayer(dData: DPlayerData1) {
         setDPlayer(dData.toDPlayerData2(), dData.uuid)
     }
 
+    /**
+     * Set a DPlayer from dPlayerData of the for [DPlayerData2] and the player UUID
+     * @param dData The dPlayerData of the player
+     * @param playerUUID The UUID of the player
+     */
     override fun setDPlayer(dData: DPlayerData2, playerUUID: UUID) {
         val path = "$dPlayerDataPath.$playerUUID"
 
@@ -70,6 +99,12 @@ class YMLDataSource(var filePath: String, val bukkitWrapper: BukkitWrapper = Buk
         saveMap("$path.$groupXpName", dData.groupXps)
     }
 
+    /**
+     * Convert a configuration section and an UUID to a [DPlayer], returns null if
+     * the section is incomplete
+     * @param configurationSection The config section to parse
+     * @param playerUUID The UUID for the [DPlayer]
+     */
     private fun configSectionToDPlayer(configurationSection: ConfigurationSection, playerUUID: UUID): DPlayer?{
 
         val job = configurationSection.getString(jobName)
@@ -86,6 +121,12 @@ class YMLDataSource(var filePath: String, val bukkitWrapper: BukkitWrapper = Buk
         return DPlayerFactory.createDPlayer(playerUUID, job, wanted, isCriminal, groupLvls, groupXps)
     }
 
+    /**
+     * Convert a configuration section to a map of the type [K], [V]
+     * @param K The type of the keys
+     * @param V The type of the Values
+     * @param configurationSection The config section to read from
+     */
     private fun <K, V> configSectionToMap(configurationSection: ConfigurationSection): Map<K, V> {
         val output = emptyMap<K, V>().toMutableMap()
         for (key in configurationSection.getKeys(false)){
@@ -95,6 +136,11 @@ class YMLDataSource(var filePath: String, val bukkitWrapper: BukkitWrapper = Buk
         return output
     }
 
+    /**
+     * Save a map to the given Path
+     * @param path The path to save the map under
+     * @param map The map to save
+     */
     private fun saveMap(path: String, map: Map<String, Any>){
         for (key in map.keys){
             dataFile.set(key, map[key])
