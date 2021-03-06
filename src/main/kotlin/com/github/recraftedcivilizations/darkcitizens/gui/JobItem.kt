552b2b2e0700fc231d8329860523a1feeeb09a3e
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-03-06 15:57:50 +0100

    feat: Open a leave job gui if the player is in the job and clicks it again

--- src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/gui/JobItem.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/gui/JobItem.kt
@@ -1,18 +1,47 @@
 package com.github.recraftedcivilizations.darkcitizens.gui
 
 import com.github.darkvanityoflight.recraftedcore.gui.Clickable
+import com.github.darkvanityoflight.recraftedcore.gui.GUIManager
+import com.github.darkvanityoflight.recraftedcore.gui.InventoryGUI
 import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayerManager
 import com.github.recraftedcivilizations.darkcitizens.jobs.IJob
 import com.github.recraftedcivilizations.darkcitizens.jobs.JobManager
+import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
+private class YesItem(itemStack: ItemStack, val job: IJob) : Clickable(itemStack) {
+    override fun onClick(player: Player) {
+        job.leave(player)
+        GUIManager.forceClose(player)
+    }
+}
+
+private class NoItem(itemStack: ItemStack): Clickable(itemStack){
+    override fun onClick(player: Player) {
+        GUIManager.forceClose(player)
+    }
+
+}
+
 class JobItem(itemStack: ItemStack, val job: IJob, val dPlayerManager: DPlayerManager, val jobManager: JobManager) : Clickable(itemStack) {
+    val leaveJobGUI: InventoryGUI = InventoryGUI(9, "Do you want to leave your current job")
+
+    init {
+        leaveJobGUI.setSlot(YesItem(ItemStack(Material.GREEN_WOOL), job), 3)
+        leaveJobGUI.setSlot(NoItem(ItemStack(Material.RED_WOOL)), 5)
+    }
+
+
     override fun onClick(player: Player) {
         val dPlayer = dPlayerManager.getDPlayer(player)
 
         if (dPlayer != null) {
-            job.join(dPlayer)
+            if (job.isMember(dPlayer)){
+                leaveJobGUI.show(player)
+            }else{
+                job.join(dPlayer)
+            }
         }else{
             player.sendMessage("Yeah uhm this is kinda awkward, an unusual error occurred please try again or ask someone who has a clue")
             throw NullPointerException()
