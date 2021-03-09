a459f270fb2a955828785c3d49f0ade2ef8b2a7a
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-03-09 17:17:06 +0100

    feat: Create a BaseIncomeRunner to pay out every base income to everyone in every job

---
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/runnables/BaseIncomeRunner.kt
@@ -0,0 +1,25 @@
+package com.github.recraftedcivilizations.darkcitizens.runnables
+
+import com.github.recraftedcivilizations.darkcitizens.BukkitWrapper
+import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayerManager
+import com.github.recraftedcivilizations.darkcitizens.jobs.JobManager
+import net.milkbowl.vault.economy.Economy
+import org.bukkit.scheduler.BukkitRunnable
+
+class BaseIncomeRunner(val jobManager: JobManager, private val playerManager: DPlayerManager, private val economy: Economy, private val bukkitWrapper: BukkitWrapper = BukkitWrapper()) :
+    BukkitRunnable() {
+    override fun run() {
+
+        for (job in jobManager.getJobs()){
+            val money = job.baseIncome
+            val xp = job.baseXPGain
+            val players = job.currentMembers
+
+            for(player in players){
+                player.addXP(job.group, xp)
+                economy.depositPlayer(bukkitWrapper.getPlayer(player.uuid), money.toDouble())
+                playerManager.setDPlayer(player)
+            }
+        }
+    }
+}
