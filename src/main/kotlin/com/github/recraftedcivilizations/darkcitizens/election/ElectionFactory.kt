d8093bfd03a4d514a54cb8d9e8fd5023d8690510
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-03-25 12:37:49 +0100

    chore: Pass down bukkit wrapper

--- src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/election/ElectionFactory.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/election/ElectionFactory.kt
@@ -1,12 +1,17 @@
 package com.github.recraftedcivilizations.darkcitizens.election
 
+import com.github.recraftedcivilizations.darkcitizens.BukkitWrapper
 import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayerManager
 import com.github.recraftedcivilizations.darkcitizens.jobs.IJob
 import net.milkbowl.vault.economy.Economy
 
 object ElectionFactory {
 
-    fun createElection(electTime: Int, job: IJob, voteFee: Int, candidateFee: Int, dPlayerManager: DPlayerManager, economy: Economy): IElect{
-        return GUIElection(electTime, job, voteFee, candidateFee, dPlayerManager, economy)
+    fun createElection(electTime: Int, job: IJob, voteFee: Int, candidateFee: Int, dPlayerManager: DPlayerManager, economy: Economy, bukkitWrapper: BukkitWrapper? = null): IElect{
+        return if (bukkitWrapper != null){
+            GUIElection(electTime, job, voteFee, candidateFee, dPlayerManager, economy, bukkitWrapper)
+        }else{
+            GUIElection(electTime, job, voteFee, candidateFee, dPlayerManager, economy)
+        }
     }
 }
