9aaac56420ed7deb4f43e9103d79ff4cf3bca64f
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-03-27 13:50:32 +0100

    feat: Create an ElectedJob class for jobs that get elected

---
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/jobs/ElectedJob.kt
@@ -0,0 +1,44 @@
+package com.github.recraftedcivilizations.darkcitizens.jobs
+
+import com.github.recraftedcivilizations.darkcitizens.BukkitWrapper
+import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayer
+import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayerManager
+import com.github.recraftedcivilizations.darkcitizens.tasks.ITask
+import org.bukkit.ChatColor
+import org.bukkit.Material
+
+class ElectedJob(
+    name: String,
+    group: String,
+    playerLimit: Int,
+    tasks: Set<ITask>,
+    canDemote: Set<String>,
+    baseIncome: Int,
+    baseXPGain: Int,
+    minLvl: Int,
+    permissionRequired: Boolean,
+    icon: Material,
+    dPlayerManager: DPlayerManager,
+    jobManager: JobManager,
+    private val bukkitWrapper: BukkitWrapper = BukkitWrapper()
+) : GenericJob(
+    name,
+    group,
+    playerLimit,
+    tasks,
+    canDemote,
+    baseIncome,
+    baseXPGain,
+    minLvl,
+    permissionRequired,
+    icon,
+    dPlayerManager,
+    jobManager,
+    bukkitWrapper
+) {
+    override fun join(dPlayer: DPlayer) {
+        val player = bukkitWrapper.getPlayer(dPlayer.uuid)!!
+        addPlayer(dPlayer)
+        player.sendMessage("${ChatColor.GREEN}You are now a $name")
+    }
+}
