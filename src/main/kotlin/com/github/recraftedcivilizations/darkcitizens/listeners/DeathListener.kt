185d939141b231d774fdbe14c412f5235a85cba7
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-03-29 14:13:16 +0200

    feat: Leave the job on death if leaveOnDeath property is set and trigger new elections

---
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/listeners/DeathListener.kt
@@ -0,0 +1,28 @@
+package com.github.recraftedcivilizations.darkcitizens.listeners
+
+import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayerManager
+import com.github.recraftedcivilizations.darkcitizens.election.ElectionManager
+import com.github.recraftedcivilizations.darkcitizens.jobs.ElectedJob
+import com.github.recraftedcivilizations.darkcitizens.jobs.JobManager
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
+
+class DeathListener(private val dPlayerManager: DPlayerManager, private val jobManager: JobManager, private val electionManager: ElectionManager) : Listener {
+
+    @EventHandler(ignoreCancelled = true)
+    fun onDeath(e: PlayerDeathEvent){
+        val player = e.entity
+        val dPlayer = dPlayerManager.getDPlayer(player)!!
+
+        val job = jobManager.getJob(dPlayer.job) ?: return
+
+        // Leave the job if the leave on death option is set
+        if (job.leaveOnDeath){
+            job.leave(dPlayer)
+            // If the job is elected trigger a new election
+            if (job is ElectedJob)  electionManager.createElection(job)
+        }
+
+    }
+}
