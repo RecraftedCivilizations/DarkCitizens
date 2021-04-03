808e26c702dc1bcdb8ec262670285e340ece0b3e
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-04-03 19:59:24 +0200

    feat: Create a Custom Law class the major will be able to set

---
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/laws/CustomLaw.kt
@@ -0,0 +1,11 @@
+package com.github.recraftedcivilizations.darkcitizens.laws
+
+/**
+ * @author DarkVanityOfLight
+ */
+
+/**
+ * Represents a custom law the major can set
+ * @param description The description of the law
+ */
+class CustomLaw(override val description: String) : ILaw
