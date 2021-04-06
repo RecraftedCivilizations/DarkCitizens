859b5a9c5028f3281bd4f09b8d0f825a10852017
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-04-06 14:03:50 +0200

    fix: Add params to UnelectedMajor

--- src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/jobs/special/major/UnelectedMajor.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/jobs/special/major/UnelectedMajor.kt
@@ -8,6 +8,37 @@ import com.github.recraftedcivilizations.darkcitizens.laws.LawManager
 import com.github.recraftedcivilizations.darkcitizens.tasks.ITask
 import org.bukkit.Material
 
-class UnelectedMajor(lawManager: LawManager) : GenericMajor(lawManager){
+class UnelectedMajor(lawManager: LawManager,
+                     name: String,
+                     group: String,
+                     playerLimit: Int,
+                     tasks: Set<ITask>,
+                     canDemote: Set<String>,
+                     baseIncome: Int,
+                     baseXPGain: Int,
+                     minLvl: Int,
+                     permissionRequired: Boolean,
+                     icon: Material,
+                     leaveOnDeath: Boolean,
+                     dPlayerManager: DPlayerManager,
+                     jobManager: JobManager,
+                     bukkitWrapper: BukkitWrapper = BukkitWrapper()
+) : GenericMajor(
+    lawManager,
+    name,
+    group,
+    playerLimit,
+    tasks,
+    canDemote,
+    baseIncome,
+    baseXPGain,
+    minLvl,
+    permissionRequired,
+    icon,
+    leaveOnDeath,
+    dPlayerManager,
+    jobManager,
+    bukkitWrapper
+) {
 
 }
