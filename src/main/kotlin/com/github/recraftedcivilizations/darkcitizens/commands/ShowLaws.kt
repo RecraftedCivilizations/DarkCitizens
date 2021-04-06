2c1d7ac5205d33ac8364e0996d0cca5366e50b2c
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-04-06 17:56:13 +0200

    feat: Add a command to get a law book with all laws

---
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/commands/ShowLaws.kt
@@ -0,0 +1,37 @@
+package com.github.recraftedcivilizations.darkcitizens.commands
+
+import com.github.recraftedcivilizations.darkcitizens.laws.LawManager
+import org.bukkit.Material
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.BookMeta
+
+class ShowLaws(private val lawManager: LawManager): CommandExecutor {
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (sender !is Player) {
+            sender.sendMessage("Fuck off console man!!"); return false
+        }
+
+        val laws = lawManager.getLaws().toTypedArray()
+        val lawBook = ItemStack(Material.WRITTEN_BOOK)
+        val lawBookMeta = lawBook.itemMeta as BookMeta
+        lawBookMeta.title = "Laws"
+        lawBookMeta.author = "The Government"
+
+        val pages = List<String>(laws.size) { i ->
+            "${laws[i].name}\n${laws[i].description}" }
+
+        lawBookMeta.pages = pages
+
+        lawBook.itemMeta = lawBookMeta
+        sender.inventory.addItem(lawBook)
+        return true
+
+    }
+
+
+}
