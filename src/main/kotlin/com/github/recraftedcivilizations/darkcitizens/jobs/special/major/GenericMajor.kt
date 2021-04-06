3f532c889f1271bec6cc9135d6eb29b3819c05ec
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-04-06 13:58:05 +0200

    feat: Create the major job

---
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/jobs/special/major/GenericMajor.kt
@@ -0,0 +1,56 @@
+package com.github.recraftedcivilizations.darkcitizens.jobs.special.major
+
+
+import com.github.recraftedcivilizations.darkcitizens.BukkitWrapper
+import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayerManager
+import com.github.recraftedcivilizations.darkcitizens.jobs.GenericJob
+import com.github.recraftedcivilizations.darkcitizens.jobs.IJob
+import com.github.recraftedcivilizations.darkcitizens.jobs.JobManager
+import com.github.recraftedcivilizations.darkcitizens.laws.LawManager
+import com.github.recraftedcivilizations.darkcitizens.tasks.ITask
+import org.bukkit.Material
+
+abstract class GenericMajor(override val lawManager: LawManager,
+                            name: String,
+                            group: String,
+                            playerLimit: Int,
+                            tasks: Set<ITask>,
+                            canDemote: Set<String>,
+                            baseIncome: Int,
+                            baseXPGain: Int,
+                            minLvl: Int,
+                            permissionRequired: Boolean,
+                            icon: Material,
+                            leaveOnDeath: Boolean,
+                            dPlayerManager: DPlayerManager,
+                            jobManager: JobManager,
+                            bukkitWrapper: BukkitWrapper = BukkitWrapper()
+): IMajor, GenericJob(
+    name,
+    group,
+    playerLimit,
+    tasks,
+    canDemote,
+    baseIncome,
+    baseXPGain,
+    minLvl,
+    permissionRequired,
+    icon,
+    leaveOnDeath,
+    dPlayerManager,
+    jobManager,
+    bukkitWrapper
+) {
+
+    override fun createLaw(name: String, description: String) {
+        lawManager.createLaw(name, description)
+    }
+
+    override fun removeLaw(name: String) {
+        lawManager.removeLaw(name)
+    }
+
+    override fun setTaxLaw(amount: Int) {
+        lawManager.setTaxAmount(amount)
+    }
+}
---
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/jobs/special/major/UnelectedMajor.kt
@@ -0,0 +1,13 @@
+package com.github.recraftedcivilizations.darkcitizens.jobs.special.major
+
+import com.github.recraftedcivilizations.darkcitizens.BukkitWrapper
+import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayerManager
+import com.github.recraftedcivilizations.darkcitizens.jobs.GenericJob
+import com.github.recraftedcivilizations.darkcitizens.jobs.JobManager
+import com.github.recraftedcivilizations.darkcitizens.laws.LawManager
+import com.github.recraftedcivilizations.darkcitizens.tasks.ITask
+import org.bukkit.Material
+
+class UnelectedMajor(lawManager: LawManager) : GenericMajor(lawManager){
+
+}
