49b128ff1d76b53b7b629011c2c731db1bdaa55c
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-04-22 18:06:24 +0200

    feat: Send message if you are demote by inv gui

--- src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/gui/DemoteItem.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/gui/DemoteItem.kt
@@ -2,17 +2,20 @@ package com.github.recraftedcivilizations.darkcitizens.gui
 
 import com.github.darkvanityoflight.recraftedcore.gui.Clickable
 import com.github.darkvanityoflight.recraftedcore.gui.DisplayItem
+import com.github.recraftedcivilizations.darkcitizens.BukkitWrapper
 import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayer
 import com.github.recraftedcivilizations.darkcitizens.jobs.IJob
+import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
-class DemoteItem(itemStack: ItemStack, val owner: DPlayer, val job: IJob) : Clickable(itemStack) {
+class DemoteItem(itemStack: ItemStack, val owner: DPlayer, val job: IJob, private val bukkitWrapper: BukkitWrapper = BukkitWrapper()) : Clickable(itemStack) {
     override fun clone(): DisplayItem {
         return DemoteItem(itemStack, owner, job)
     }
 
     override fun onClick(player: Player) {
         job.leave(this.owner)
+        bukkitWrapper.getPlayer(owner)!!.sendMessage("${ChatColor.RED}You got demoted by ${player.name}")
     }
 }
