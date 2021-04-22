61bb03ff103a42319b26f8d303a431104e827632
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-04-22 18:00:12 +0200

    feat: Demote players through a GUI

--- src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/commands/Demote.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/commands/Demote.kt
@@ -1,14 +1,20 @@
 package com.github.recraftedcivilizations.darkcitizens.commands
 
 
+import com.github.darkvanityoflight.recraftedcore.gui.InventoryGUI
+import com.github.darkvanityoflight.recraftedcore.utils.itemutils.setName
 import com.github.recraftedcivilizations.darkcitizens.BukkitWrapper
 import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayerManager
+import com.github.recraftedcivilizations.darkcitizens.gui.VoteItem
 import com.github.recraftedcivilizations.darkcitizens.jobs.JobManager
 import org.bukkit.ChatColor
+import org.bukkit.Material
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.SkullMeta
 
 class Demote(private val dPlayerManager: DPlayerManager, private val jobManager: JobManager, private val bukkitWrapper: BukkitWrapper = BukkitWrapper()): CommandExecutor {
 
@@ -34,6 +40,50 @@ class Demote(private val dPlayerManager: DPlayerManager, private val jobManager:
             sender.sendMessage("${ChatColor.RED}You need to define a player to demote!")
             return false
         }
+        val senderJob = jobManager.getJob(dPlayerManager.getDPlayer(sender)?.job)
+
+        // Check if the demote arg is a job if yes display all members and make it
+        // possible to demote them
+        val demoteJob = jobManager.getJob(args[0])
+        if (demoteJob != null){
+            if (senderJob != null){
+                if (demoteJob.name in senderJob.canDemote){
+
+                    val invGui = InventoryGUI(demoteJob.currentMembers.size, "Demote")
+
+                    for (member in demoteJob.currentMembers){
+                        val toDemotePlayer = bukkitWrapper.getPlayer(member)
+                        // Set the head skin
+                        val displayItemStack = ItemStack(Material.PLAYER_HEAD, 1)
+                        val skullMeta = displayItemStack.itemMeta as SkullMeta
+                        skullMeta.owningPlayer = toDemotePlayer
+                        displayItemStack.itemMeta = skullMeta
+
+                        displayItemStack.setName(toDemotePlayer?.name)
+
+                        //val displayItem = DemoteItem()
+                        //invGui.addItem(displayItem)
+                    }
+
+                    invGui.show(sender)
+                    return true
+                }
+            }
+        }
+
+
+        /*
+        // Set the head skin
+        val displayItemStack = ItemStack(Material.PLAYER_HEAD, 1)
+        val skullMeta = displayItemStack.itemMeta as SkullMeta
+        skullMeta.owningPlayer = candidatePlayer
+        displayItemStack.itemMeta = skullMeta
+
+        displayItemStack.setName(candidatePlayer?.name)
+
+        val displayItem = VoteItem(displayItemStack, this, candidate, dPlayerManager)
+        invGUI.addItem(displayItem)*/
+
 
         // Get the target
         val target = bukkitWrapper.getPlayer(args[0])
@@ -46,7 +96,6 @@ class Demote(private val dPlayerManager: DPlayerManager, private val jobManager:
 
         // Get the jobs
         val targetJob = dPlayerManager.getDPlayer(target)?.job
-        val senderJob = jobManager.getJob(dPlayerManager.getDPlayer(sender)?.job)
 
 
         // Demote if the permissions are okay
