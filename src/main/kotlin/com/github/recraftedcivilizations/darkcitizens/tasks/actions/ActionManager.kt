014b1068d150306011a7654cd8c4e37071dfde80
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-05-10 22:32:35 +0200

    chore: Docs

--- src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/tasks/actions/ActionManager.kt
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/tasks/actions/ActionManager.kt
@@ -1,15 +1,28 @@
 package com.github.recraftedcivilizations.darkcitizens.tasks.actions
 
 /**
+ * @author DarkVanityOfLight
+ */
+
+/**
  * Register new actions here and get registered ones
  */
 object ActionManager {
     val actions: MutableSet<IAction> = emptySet<IAction>().toMutableSet()
 
+    /**
+     * Register a new action
+     * @param action The action to register
+     */
     fun registerAction(action: IAction){
         actions.add(action)
     }
 
+    /**
+     * Get a registered action
+     * @param name The name of the action
+     * @return The action or null if nothing is found
+     */
     fun getAction(name: String?): IAction?{
         for (action in actions){
             if (action.name == name){
