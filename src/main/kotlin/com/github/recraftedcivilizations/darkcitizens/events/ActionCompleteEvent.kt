b0501b41289a0d70395a5e8ed0bc8ae595eb25d9
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-05-12 13:33:20 +0200

    feat: Create an action complete event

---
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/events/ActionCompleteEvent.kt
@@ -0,0 +1,39 @@
+package com.github.recraftedcivilizations.darkcitizens.events
+
+import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayer
+import com.github.recraftedcivilizations.darkcitizens.jobs.IJob
+import com.github.recraftedcivilizations.darkcitizens.tasks.actions.IAction
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+
+/**
+ * @author DarkVanityOfLight
+ */
+
+/**
+ * An event that gets emitted when a player leaves his job
+ * @param dPlayer The player who leaves his job
+ * @param action The completed action
+ */
+class ActionCompleteEvent(
+    val dPlayer: DPlayer,
+    val action: IAction
+): Event() {
+
+    companion object{
+        private val HANDLER_LIST: HandlerList = HandlerList()
+
+        @JvmStatic
+        fun getHandlerList(): HandlerList {
+            return HANDLER_LIST
+        }
+    }
+
+    /**
+     * Get all handlers
+     * @return The HandlerList
+     */
+    override fun getHandlers(): HandlerList {
+        return HANDLER_LIST
+    }
+}
