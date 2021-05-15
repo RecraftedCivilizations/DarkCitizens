c7b18032ecc8d232a54110abbc565a73da894308
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-05-15 11:59:15 +0200

    feat: Implement an action that checks if Items are crafted

---
+++ src/main/kotlin/com/github/recraftedcivilizations/darkcitizens/actions/actions/CraftItem.kt
@@ -0,0 +1,69 @@
+package com.github.recraftedcivilizations.darkcitizens.actions.actions
+
+import com.github.recraftedcivilizations.darkcitizens.BukkitWrapper
+import com.github.recraftedcivilizations.darkcitizens.actions.Action
+import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayer
+import com.github.recraftedcivilizations.darkcitizens.dPlayer.DPlayerManager
+import com.github.recraftedcivilizations.darkcitizens.dPlayer.inc
+import com.github.recraftedcivilizations.darkcitizens.events.ActionCompleteEvent
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.CraftItemEvent
+import org.bukkit.event.inventory.InventoryType
+import java.util.*
+
+/**
+ * @author DarkVanity
+ * TODO("Reset only the required number of items")
+ */
+
+class CraftItem(val number: Int, val itemType: Material, override val name: String, override val description: String, private val dPlayerManager: DPlayerManager, private val bukkitWrapper: BukkitWrapper = BukkitWrapper()) : Action(), Listener {
+    private val storage: MutableMap<UUID, Int> = emptyMap<UUID, Int>().toMutableMap()
+
+    @EventHandler(ignoreCancelled = true)
+    fun onCrafting(e: CraftItemEvent){
+        val crafter = e.whoClicked
+        if (crafter !is Player) return
+
+        val dPlayer = dPlayerManager.getDPlayer(crafter)!!
+
+        if (e.slotType == InventoryType.SlotType.RESULT){
+            val result = e.recipe.result
+            storage.inc(crafter.uniqueId, result.amount)
+            val surplus = (storage[crafter.uniqueId]?: 0) - number
+            if (surplus >= 0 ){
+                val completeEvent = ActionCompleteEvent(dPlayer, this)
+                bukkitWrapper.getPluginManager().callEvent(completeEvent)
+            }
+
+        }
+
+    }
+
+
+    override fun isCompletedForPlayer(player: DPlayer): Boolean {
+        return isCompletedForPlayer(player.uuid)
+    }
+
+    override fun isCompletedForPlayer(player: Player): Boolean {
+        return isCompletedForPlayer(player.uniqueId)
+    }
+
+    private fun isCompletedForPlayer(uuid: UUID): Boolean{
+        return storage[uuid]?:0 >= number
+    }
+
+    override fun resetForPlayer(player: Player) {
+        resetForPlayer(player.uniqueId)
+    }
+
+    override fun resetForPlayer(player: DPlayer) {
+        resetForPlayer(player.uuid)
+    }
+
+    private fun resetForPlayer(uuid: UUID){
+        storage[uuid] = 0
+    }
+}
