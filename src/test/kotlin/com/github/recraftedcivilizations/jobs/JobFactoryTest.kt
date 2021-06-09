47211c7214bd1787114b77d14e10674905724d5c
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-06-09 14:19:39 +0200

    feat: Test the creation of major jobs

--- src/test/kotlin/com/github/recraftedcivilizations/jobs/JobFactoryTest.kt
+++ src/test/kotlin/com/github/recraftedcivilizations/jobs/JobFactoryTest.kt
@@ -7,6 +7,9 @@ import com.github.recraftedcivilizations.darkcitizens.jobs.Job
 import com.github.recraftedcivilizations.darkcitizens.jobs.JobFactory
 import com.github.recraftedcivilizations.darkcitizens.jobs.JobManager
 import com.github.recraftedcivilizations.darkcitizens.jobs.elected.GenericElectedJob
+import com.github.recraftedcivilizations.darkcitizens.jobs.special.mayor.ElectedMayor
+import com.github.recraftedcivilizations.darkcitizens.jobs.special.mayor.UnelectedMayor
+import com.github.recraftedcivilizations.darkcitizens.laws.LawManager
 import com.nhaarman.mockitokotlin2.mock
 import org.bukkit.Material
 import org.junit.jupiter.api.Test
@@ -61,5 +64,46 @@ internal class JobFactoryTest {
         assertEquals(job.permissionRequired, thatJob.permissionRequired)
         assertEquals(job.icon, thatJob.icon)
         assertEquals(true, thatJob is GenericElectedJob)
+    }
+
+    @Test
+    fun createElectedMajorJob(){
+
+        val lawManager = mock<LawManager>{}
+
+        val icon = mock<Material>{}
+        val job = createRandomGenericElectedJob(mock{}, icon, dPlayerManager, jobManager)
+        val thatJob = JobFactory.createJob(job.name, job.group, job.playerLimit, job.tasks, job.canDemote, job.baseIncome, job.baseXPGain, job.minLvl, true, job.permissionRequired, job.icon, job.leaveOnDeath, job.prefix, dPlayerManager, jobManager, job.candidateTime, job.voteTime, job.candidateFee, job.voteFee, true, lawManager)
+        assertEquals(job.name, thatJob.name)
+        assertEquals(job.group, thatJob.group)
+        assertEquals(job.playerLimit, thatJob.playerLimit)
+        assertEquals(job.canDemote, thatJob.canDemote)
+        assertEquals(job.baseIncome, thatJob.baseIncome)
+        assertEquals(job.baseXPGain, thatJob.baseXPGain)
+        assertEquals(job.minLvl, thatJob.minLvl)
+        assertEquals(job.permissionRequired, thatJob.permissionRequired)
+        assertEquals(job.icon, thatJob.icon)
+        assertEquals(true, thatJob is ElectedMayor)
+
+    }
+
+    @Test
+    fun createUnelectedMajorJob(){
+
+        val lawManager = mock<LawManager>{}
+
+        val icon = mock<Material>{}
+        val job = createRandomGenericElectedJob(mock{}, icon, dPlayerManager, jobManager)
+        val thatJob = JobFactory.createJob(job.name, job.group, job.playerLimit, job.tasks, job.canDemote, job.baseIncome, job.baseXPGain, job.minLvl, false, job.permissionRequired, job.icon, job.leaveOnDeath, job.prefix, dPlayerManager, jobManager, job.candidateTime, job.voteTime, job.candidateFee, job.voteFee, true, lawManager)
+        assertEquals(job.name, thatJob.name)
+        assertEquals(job.group, thatJob.group)
+        assertEquals(job.playerLimit, thatJob.playerLimit)
+        assertEquals(job.canDemote, thatJob.canDemote)
+        assertEquals(job.baseIncome, thatJob.baseIncome)
+        assertEquals(job.baseXPGain, thatJob.baseXPGain)
+        assertEquals(job.minLvl, thatJob.minLvl)
+        assertEquals(job.permissionRequired, thatJob.permissionRequired)
+        assertEquals(job.icon, thatJob.icon)
+        assertEquals(true, thatJob is UnelectedMayor)
     }
 }
--- src/test/kotlin/com/github/recraftedcivilizations/jobs/JobTestInfrastructure.kt
+++ src/test/kotlin/com/github/recraftedcivilizations/jobs/JobTestInfrastructure.kt
@@ -6,6 +6,9 @@ import com.github.recraftedcivilizations.darkcitizens.jobs.IJob
 import com.github.recraftedcivilizations.darkcitizens.jobs.Job
 import com.github.recraftedcivilizations.darkcitizens.jobs.JobManager
 import com.github.recraftedcivilizations.darkcitizens.jobs.elected.GenericElectedJob
+import com.github.recraftedcivilizations.darkcitizens.jobs.special.mayor.ElectedMayor
+import com.github.recraftedcivilizations.darkcitizens.jobs.special.mayor.UnelectedMayor
+import com.github.recraftedcivilizations.darkcitizens.laws.LawManager
 import org.bukkit.Material
 import kotlin.random.Random
 import kotlin.random.nextInt
@@ -16,4 +19,12 @@ fun createRandomJob(bukkitWrapper: BukkitWrapper, icon: Material, dPlayerManager
 
 fun createRandomGenericElectedJob(bukkitWrapper: BukkitWrapper, icon: Material, dPlayerManager: DPlayerManager, jobManager: JobManager): GenericElectedJob{
     return GenericElectedJob(Random.nextBoolean(), Random.nextInt(), Random.nextInt(), Random.nextInt(), Random.nextInt(), dPlayerManager, randomString(), randomString(), Random.nextInt(), emptySet(), emptySet(), Random.nextInt(), Random.nextInt(), Random.nextInt(), Random.nextBoolean(), icon, randomString(), jobManager)
+}
+
+fun  createRandomElectedMajorJob(bukkitWrapper: BukkitWrapper, icon: Material, dPlayerManager: DPlayerManager, jobManager: JobManager, lawManager: LawManager): ElectedMayor{
+    return ElectedMayor(lawManager, randomString(), randomString(), Random.nextInt(1..10), emptySet(), emptySet(), Random.nextInt(), Random.nextInt(), 0, false, icon, Random.nextBoolean(), randomString(), dPlayerManager, jobManager, Random.nextInt(), Random.nextInt(), Random.nextInt(), Random.nextInt(), bukkitWrapper)
+}
+
+fun createRandomUnelectedMajorJob(bukkitWrapper: BukkitWrapper, icon: Material, dPlayerManager: DPlayerManager, jobManager: JobManager, lawManager: LawManager): UnelectedMayor{
+    return UnelectedMayor(lawManager, randomString(), randomString(), Random.nextInt(1..10), emptySet(), emptySet(), Random.nextInt(), Random.nextInt(), 0, false, icon, Random.nextBoolean(), randomString(), dPlayerManager, jobManager, bukkitWrapper)
 }
